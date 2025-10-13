c6ee0b65aabdc140108555d8daf325c98f293429
Shen327 <ideallab.zs@gmail.com>
2025-10-13 15:40:29 +0800

    Delete api directory

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-export const config = { runtime: 'edge' };
-
-export default async function handler() {
-  return new Response('pong', {
-    status: 200,
-    headers: { 'content-type': 'text/plain' }
-  });
-}
